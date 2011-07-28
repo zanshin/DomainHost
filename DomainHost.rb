12a856a471e822c698e69af2a8d887bee62fdebf
Mark Nichols <mark@zanshin.net>
2011-07-27 22:20:57 -0500

    not sure I understand all I know about this

--- DomainHost.rb
+++ DomainHost.rb
@@ -1,12 +1,57 @@
-counter = 1
-begin
-	file = File.new("chromeBookmarks.html", "r")
-	while (line = file.gets)
-		puts "#{counter}: #{line}"
-		counter = counter + 1
-	end
-	file.close
-rescue => err
-	puts "Exception: #{err}"
-	err
+#!/usr/bin/env ruby
+
+#
+# DomainHost.rb
+#
+# This application parses a Netscape fomatted bookmarks file and produces a list of the domain
+# hosts used by the domain contained in the file. Each domain is only counted once, regardless of
+# how many times it or derevations of it occur in the file.
+#
+
+require "uri"
+
+# -------------------------------------------------
+# DomainHost is our main
+# -------------------------------------------------
+class DomainHost
+  counter = 1
+  def read_bookmarks(bookmarks)
+    begin
+    	file = File.new(bookmarks, "r")
+    	while (line = file.gets)
+    		#puts "#{counter}: #{line}"
+		
+    		# extract the URL
+    		aUrl = URI.extract(line)
+    		puts aUrl
+    		#theUrl = URI.parse(aUrl.to_s)
+    		#domain = theUrl.host 
+    		validate_url(aUrl)
+    		#puts "#{counter}: #{domain}"
+    		counter = counter + 1
+    	end
+    	file.close
+    rescue => err
+    	puts "Exception: #{err}"
+    	err
+    end
+  end
+
+  # -------------------------------------------------
+  # validate_url looks for a valid URL
+  # -------------------------------------------------
+  def validate_url(url)
+    uri = URI.parse(url)
+    uri.class != URI::HTTP
+  rescue URI::InvalidURIError
+    false
+  end
+
 end
+  
+  # -------------------------------------------------
+  # call read_bookmarks with a file
+  # -------------------------------------------------
+  read_bookmarks("chromeBookmarks")
+
+
